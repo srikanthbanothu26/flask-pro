a0d77437479bb886efcd465785cee03832b44253
banothusrikanth <banothusrikanth267@gmail.com>
2024-12-04 21:41:10 +0530

    removed tailwindcss installation

--- static/src/js/home.js
+++ static/src/js/home.js
@@ -1,2 +1,10 @@
-var hello = document.getElementById('name').innerText;
-console.log(hello);
+
+function getName() {
+    var hello = document.getElementById('name').innerText;
+  
+    console.log(hello);
+
+    return hello;
+}
+
+getName();
